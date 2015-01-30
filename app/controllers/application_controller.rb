c6f7643c390c0fd8b1f2d9b74216007744bffb95
Van Nguyen <soutennizavan@gmail.com>
2015-01-30 14:12:32 -0500

    fixed bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render text: "Thug Life"    
+    render text: "Thug Life"
+  end 
 end
